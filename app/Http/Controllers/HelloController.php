f3eadb77982498d1b3ba1ce6ca1fe07c186b2fe3
joaquin@oktana.io <joaquingimenez83@gmail.com>
2019-10-01 15:03:56 -0300

    added db

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use App\User;
+use Illuminate\Database\Eloquent\Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class HelloController extends Controller
 {
-    //
+    public const MODEL = User::class;
+
+    // use RESTActions;
+
+    public function get($name): ?\Illuminate\Http\JsonResponse
+    {
+        $m = static::MODEL;
+        
+        try {
+            $newuser = $m::create([
+                'email' => $name . '@gmail.com',
+                'name' => $name,
+                'password' => '1234'
+            ]);
+            $user = $m::where('name', $name)->firstOrFail();
+                return $this->respond(200, $user);
+        } catch (Exception $e) {
+            return $this->respond(404);
+        } catch (ModelNotFoundException $e) {
+            return $this->respond(404);
+        }
+    }
+
+    public function getMessage($name)
+    {
+        return $this->generateMessage($name);
+        // return view('hello', [ 'name' => $this->generateMessage('Bob') ]);
+    }
+
+    public function generateMessage($name) {
+        return "Hello " . $name . " how are you doing!";
+    }
 }
