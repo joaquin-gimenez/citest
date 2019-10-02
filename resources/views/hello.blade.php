a9f34110960be6f069fa516af6d394c8f5b25a5c
joaquin@oktana.io <joaquingimenez83@gmail.com>
2019-10-02 15:26:58 -0300

    h

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -71,7 +71,7 @@
                 <div class="title m-b-md">
                   {{ $name }}
                 </div>
-                <p>This isddddfddddsfF jsdsxustsd sasdsd a samspsdle page mfodified to test again!</p>
+                <p>This isddddfdsddddsfF jsdsxustsd sasdsd a samspsdle page mfodified to test again!</p>
             </div>
         </div>
     </body>
